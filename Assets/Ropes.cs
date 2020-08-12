2528f502b3835e4d6841e6bb74af9844268dba9b
mastfissh <mastfish@gmail.com>
2020-08-12 15:42:04 +0200

    Rework rope joints

--- Assets/Ropes.cs
+++ Assets/Ropes.cs
@@ -7,97 +7,113 @@ using System;
 [RequireComponent (typeof (Rigidbody))]
 public class Ropes : MonoBehaviour
 {
-	public Transform target;
+
+	public GameObject destination;
 	public GameObject lineHolder;
-	public float ropeDrag = 0.1F;
-	public float ropeMass = 0.1F;
-	public float ropeColRadius = 0.1F;
-	public float ropeMin = 0.05f;
-	public float ropeMax = 0.11f;
-	public float damper = 25;
-	public float springPower = 2000;
+	// all of these are total, not per segment
+	public float drag = 0.1f;
+	public float mass = 0.1f;
+	public float angular_drag = 0.5f;
+	public float width = 0.05f;
 	public int jointCount = 5;
-	private Vector3[] segmentPos;
-	private GameObject[] joints;
-	private int segments = 0;
- 	private LineRenderer line;
+
+	private GameObject source;
+	private int segmentCount;
+	private ConfigurableJoint baseJoint;
+	private LineRenderer line;
+	private GameObject[] jointHolders;
+	private float ropeDrag;
+	private float ropeMass;
+	private float ropeAngDrag;
 
     // Start is called before the first frame update
     void Start()
     {
     	line = lineHolder.GetComponent<LineRenderer>();
+    	baseJoint = lineHolder.GetComponent<ConfigurableJoint>();
+    	// One to join with the destination, one to join the source
+    	segmentCount = jointCount + 2;
+		jointHolders = new GameObject[jointCount];
+		ropeDrag = drag / jointCount;
+		ropeMass = mass / jointCount;
+		ropeAngDrag = angular_drag / jointCount;
+		line.positionCount = segmentCount;
+		source = this.gameObject;
         BuildRope();
     }
 
     // Update is called once per frame
     void Update()
     {
-    	line.positionCount = segments + 2;
-    	var joint_positions = joints.Select(
-    		x => x.transform.position
-    		).ToArray();
-    	var positions = new Vector3[segments + 2];
-    	positions[0] = transform.position;
-    	Array.Copy(joint_positions, 0, positions, 1, joint_positions.Length);
-    	positions[segments + 1] = target.position;
+		var positions = new Vector3[segmentCount];
+		int idx = 0;
+		positions[idx] = source.transform.position;
+		idx++;
+		foreach (GameObject jointHolder in jointHolders)
+	    {
+    		positions[idx] = jointHolder.transform.position;
+    		idx++;
+	    }
+    	positions[idx] = destination.transform.position;
 		line.SetPositions(positions);
     }
 
     void BuildRope()
 	{
-		segments = jointCount;
-		segmentPos = new Vector3[segments];
-		joints = new GameObject[segments];
-		segmentPos[0] = transform.position;
-		segmentPos[segments-1] = target.position;
- 
-		// Find the distance between each segment
-		var segs = segments-1;
-		var separation = ((target.position - transform.position)/segs);
-
-		for(int s=0;s < segments ;s++)
+		Vector3 delta = ((destination.transform.position - transform.position)/jointCount);
+		float size = delta.magnitude;
+		GameObject last = source;
+		for(int idx=0; idx < jointCount ;idx++)
 		{
-			// Find the each segments position using the slope from above
-			Vector3 vector = (separation*s) + transform.position;	
-			segmentPos[s] = vector;
- 
-			//Add Physics to the segments
-			AddJointPhysics(s);
+			Vector3 jointPosition = (delta*(idx+1)) + transform.position;
+			var jointHolder = buildJointHolder(idx, size);
+			jointHolders[idx] = jointHolder;
+			jointHolder.transform.position = jointPosition;
+			Join(last, jointHolder);
+			last = jointHolder;
 		}
-
-		SpringJoint end = buildJoint(target.gameObject);
-		end.connectedBody = joints[joints.Length-1].transform.GetComponent<Rigidbody>();		
+		Join(last, destination);	
 	}
 
-	SpringJoint buildJoint(GameObject target){
-		SpringJoint joint = target.AddComponent<SpringJoint>();
-		joint.minDistance = ropeMin;
-		joint.maxDistance = ropeMax;
-		joint.autoConfigureConnectedAnchor = false;
-		joint.damper = damper;
-		joint.connectedAnchor = new Vector3(0,0,0);
-		joint.spring = springPower;
-		return joint;
-	}
-
-	void AddJointPhysics(int n)
+	void Join(GameObject src, GameObject dest)
 	{
-		joints[n] = new GameObject("Joint_" + n);
-		joints[n].transform.position = target.position;
-		Rigidbody rigid = joints[n].AddComponent<Rigidbody>();
-		SphereCollider col = joints[n].AddComponent<SphereCollider>();
-		SpringJoint ph = buildJoint(joints[n]);
+		var joint = buildJoint(src);
+		joint.connectedBody = dest.GetComponent<Rigidbody>();
+	}
+
+	T CopyComponent<T>(T original, GameObject destination) where T : Component
+	{
+	     System.Type type = original.GetType();
+	     Component copy = destination.AddComponent(type);
+	     System.Reflection.FieldInfo[] fields = type.GetFields();
+	     foreach (System.Reflection.FieldInfo field in fields)
+	     {
+	         field.SetValue(copy, field.GetValue(original));
+	     }
+	     return copy as T;
+	}
+
+	GameObject buildJointHolder(int idx, float size) {
+		var item = new GameObject("JointHolder_" + idx);
+		Rigidbody rigid = item.AddComponent<Rigidbody>();
+		CapsuleCollider col = item.AddComponent<CapsuleCollider>();
 		rigid.drag = ropeDrag;
 		rigid.mass = ropeMass;
-		col.radius = ropeColRadius;
- 
-		if(n==0){		
-			ph.connectedBody = transform.GetComponent<Rigidbody>();
-		} else
-		{
-			ph.connectedBody = joints[n-1].GetComponent<Rigidbody>();	
-		}
- 
+		rigid.angularDrag = ropeAngDrag;
+		rigid.solverIterations = 12;
+		col.radius = width;
+		col.height = size;
+		return item;
+	}
+
+	ConfigurableJoint buildJoint(GameObject target)
+	{
+		ConfigurableJoint joint = CopyComponent(baseJoint, target);
+		joint.xMotion = ConfigurableJointMotion.Locked;
+		joint.yMotion = ConfigurableJointMotion.Locked;
+		joint.zMotion = ConfigurableJointMotion.Locked;
+		joint.angularYMotion = ConfigurableJointMotion.Locked;
+		return joint;
 	}
 	
 }
